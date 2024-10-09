659e1f1db72129a47e9dda717ce734518866963e
TerFerre-Fan <vinn.zurnaci@student.tersterre.be>
2024-10-09 09:52:50 +0200

    evil versie
     scary

---
+++ watesigma.js
@@ -0,0 +1,8 @@
+// JavaScript Document
+
+function evil (stout) {
+	document.getElementById("text").textContent= "TOP 3 EVIL MENSEN";
+	document.getElementById("miniferre1").src="evil_mini_ferre_1.jpg";
+	document.getElementById("louis").src="evil_louis.jpg";
+	document.getElementById("johan").src="evil_johan.jpg";
+}
