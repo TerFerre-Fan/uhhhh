a27ab1dba7a995a397405af8c0e8d16c49962374
TerFerre-Fan <vinn.zurnaci@student.tersterre.be>
2024-10-16 09:52:50 +0200

    real

--- watesigma.js
+++ watesigma.js
@@ -1,8 +1,27 @@
 // JavaScript Document
 
-function evil (stout) {
-	document.getElementById("text").textContent= "TOP 3 EVIL MENSEN";
-	document.getElementById("miniferre1").src="evil_mini_ferre_1.jpg";
-	document.getElementById("louis").src="evil_louis.jpg";
-	document.getElementById("johan").src="evil_johan.jpg";
+var clicked = false;
+
+function evil () {
+
+	if (clicked == true ){
+			clicked = false;
+			document.getElementById("text").textContent= "TOP 3 MENSEN";
+		document.getElementById("miniferre1").src="mini_ferre_1.jpg";
+		document.getElementById("louis").src="louis.jpeg";
+		document.getElementById("johan").src="johan.jpg";
+	} else {
+		clicked = true;
+		document.getElementById("text").textContent= "TOP 3 EVIL MENSEN";
+		document.getElementById("miniferre1").src="evil_mini_ferre_1.jpg";
+		document.getElementById("louis").src="evil_louis.jpg";
+		document.getElementById("johan").src="evil_johan.jpg";
+	}
+	
+
+	
 }
+
+	for (var i=0; i<5; i++){
+		window.alert ("Maurice Donck wilt je locatie")
+	}
